05a8921bb84560b612faed8fa9aab9f369402d74
heygideon <47083178+heygideon@users.noreply.github.com>
2025-12-21 15:14:22 +0000

    fix(website): add astro site url

--- packages/website/astro.config.mjs
+++ packages/website/astro.config.mjs
@@ -10,6 +10,7 @@ import vercel from "@astrojs/vercel";
 // https://astro.build/config
 export default defineConfig({
   output: "server",
+  site: `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`,
 
   vite: {
     plugins: [tailwindcss()],
